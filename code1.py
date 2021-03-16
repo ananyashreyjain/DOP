b87e7b5f81656d10383efa5600e56a5c6821d007
ananyashreyjain <f20170920@goa.bits-pilani.ac.in>
2021-03-16 14:31:13 +0530

    point to FBM implemented

--- code1.py
+++ code1.py
@@ -1,6 +1,7 @@
 import numpy as np
 from termcolor import colored
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def knee_center():
 
@@ -30,7 +31,31 @@ def knee_center():
 		X1 = -60
 		Y1 = -np.sin(Varus) * X1 + ydist
 		Z1 = np.cos(Varus) * np.sin(IntRot) * X1 + zdist
+		
+def read_file(config):
+	FBM_type = int(input("Enter type number 0 - 6 \n"))
+	df = pd.read_csv(config['filename'], header = None)
+	co_ordinates = 	df.iloc[FBM_type,:]
+	co_ordinates = [float(point) for point in co_ordinates]
+	for number, point in enumerate(co_ordinates):
+		config[f'X%d'%(number+1)] = point
+		
+def points_to_FBM(config):
 
+	config['L1'] = ((config['X3'] - config['X1'])**2 + (config['X4'] - config['X2'])**2)**0.5
+	config['L2'] = ((config['X5'] - config['X3'])**2 + (config['X6'] - config['X4'])**2)**0.5
+	config['L3'] = ((config['X7'] - config['X5'])**2 + (config['X8'] - config['X6'])**2)**0.5
+	config['L4'] = ((config['X1'] - config['X7'])**2 + (config['X2'] - config['X8'])**2)**0.5
+
+	config['theta1'] = np.arctan((config['X2'] - config['X4']) / (config['X1'] - config['X3']))
+	config['theta2'] = np.arctan((config['X6'] - config['X4']) / (config['X5'] - config['X3']))
+	config['theta3'] = np.arctan((config['X8'] - config['X6']) / (config['X7'] - config['X5']))
+	
+	print(f"L1=%0.2f, L2=%0.2f, L3=%0.2f, L4=%0.2f \ntheta1=%0.2f, theta2=%0.2f, theta3=%0.2f"%
+	(config['L1'], config['L2'], config['L3'], config['L4'], 180/np.pi * config['theta1'], 
+	180/np.pi * config['theta2'], 180/np.pi * config['theta3']))
+
+	
 def _FBM_angles(config):
 
 	L1, L2, L3, L4, theta1, theta3 = config['L1'],config['L2'],\
@@ -110,21 +135,25 @@ def plot(config):
 	
 
 config = {
-		'X1':13.8,'X2':-50,'X3':-13.3,'X4':-42.8,
+		'X1':np.nan,'X2':np.nan,'X3':np.nan,'X4':np.nan,
 		'X5':np.nan,'X6':np.nan,'X7':np.nan,'X8':np.nan,
-		'L1':28.04,'L2':27.85,'L3':23.66,'L4':51.09,
-		'theta1':np.pi*-14.88/180, 'theta2':np.nan,
-		'theta3':np.pi*39.34/180, 'theta4':np.nan,
+		'L1':np.nan,'L2':np.nan,'L3':np.nan,'L4':np.nan,
+		'theta1':np.nan, 'theta2':np.nan,
+		'theta3':np.nan, 'theta4':np.nan,
 		'stance':True,
 		'draw':1,'fc': 3, 'pause':0.01, 'inc_fac': +180,
-		'frames':120
+		'frames':120, 'filename':"points.csv"
 		}
+		
+
+read_file(config)
+points_to_FBM(config)
 fig,ax = plt.subplots(1,1, figsize=(10,10))
 mx = max(config['L1'], config['L2'], config['L3'], config['L4'])
 fc = config['fc']
 plt.xlim(-fc*mx, fc*mx)
 plt.ylim(-fc*mx, fc*mx)
-centers  = []
+
 for i in range(config['frames']):
 	_FBM_angles(config)
 	if not config['draw']:
